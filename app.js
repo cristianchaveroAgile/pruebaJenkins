2674234c23998ebbc66377cd55b44bf7c70253df
CrisAmauri <cristian.chavero@agilethought.com>
2021-09-14 15:56:11 -0500

    prueba

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3001;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()
+const port = 3001
+app.get('/', (req, res) => {
+  res.send('Hola mundo!')
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
